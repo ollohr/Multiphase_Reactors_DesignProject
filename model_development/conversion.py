4403b27436fd08aa52caf38fa2a3e0db89bf9e22
Xikiee <oliverl.lohr@gmail.com>
2026-04-08 19:58:21 +0200

    o.l. update to modeliong conversion

--- model_development/conversion.py
+++ model_development/conversion.py
@@ -6,32 +6,37 @@ from scipy.integrate import solve_ivp
 
 #           General constants
 g = 9.81                        #gravitational acceleration                 [m/s2]
-# D = 0.06                        #Diffusion coefficient                    [m2/s]
 gamma = 0.375                   #Fraction of solids in the emulsion phase   [-]            idk if this should be particle hold up at mf tho... find out later (in the turbulent regime the particle hold-up is 0.3-0.45)
-# K_r=1.8                         #Reaction rate constant                   [m6/(mol2*s)]
 R = 8.3145                        # Gas constant [J/(K mol)]
+
+# K_r=1.8                         #Reaction rate constant                   [m6/(mol2*s)]
+# D = 0.06                        #Diffusion coefficient                    [m2/s]
 
 
 #           Reactor Properties
-V_fin = 100                             ########################## this is a temporary value, validate later if this value willl still hold 
+V_fin = 100                             # temporary value for integration might need to be changed later [m3]
+u_mf = 1.19e-1                          # minimum fluidization velocity [m/s]
+u_c = 0.87                              # turbulent fluidization velocity [m/s]
+
+epsilon_mf = 0.625               #Bed voidage at u_mf                        [-]
+epsilon_c  = 0.55                #Bed voidage at u_c                         [-]
+
+
 # u =                        #Superficial gas velocity                        [m/s]                       (this would only be required for the later height and volume calculation, so fill this in after)
-# mu= 1.8e-5                       #Viscosity of fluidization gas           [Pa*s]
+# A_r=np.pi*D_r**2/4              #Bottom surface area of reactor           [m2]
 # H_mf = 0.28                     #Bed height at u_mf                       [m]
 # D_r = 0.3                       #Diameter of reactor                      [m]                          
-# A_r=np.pi*D_r**2/4              #Bottom surface area of reactor           [m2]
-epsilon_mf = 0.61               #Bed voidage at u_mf                        [-]
-
 
 #           Gas properties
-# the density of gas was recalculated for p = 20 atm and T = 340 C using --> 
-rho_g = 1.94                    #Gas density                              [kg/m3]
+rho_g = 1.94                            #Gas density                              [kg/m3]
+mu= 1.54e-4                             # Dynamic viscosity of gas           [Pa*s]
 
 
 #           Catalyst properties
 rho_s = 7.794e3                   #Solid particle density                    [kg/m3]
 d_p = 100e-6                     #Particle diameter                         [m]
 phi_s= 1                    #Sphericity of catalyst                     [-]                     #actually idk if this is ok to assume, it was done earlier but check if this is ok
-
+rho_cat_bed = (1-epsilon_c)*rho_s
 
 #           Parameters from the assignment
 a0 = 8.88522e-3                # Reaction Rate Coefficient                  [mol /(s kg_cat bar2)]
@@ -40,12 +45,19 @@ b0 = 2.226                     # Adsorption coefficient at T = 493.15 K     [1/b
 DbH = -6.837e3                 # Adsorption enthalpy                        [J/mol]
 
 #           Input parameters
-F0 = np.array([])       # this is the initial flow rate
-FT = F0.sum()           # total flow rate
-p = 20 * 101325                   # pressure [Pa]
-T0 = 340 + 273.15      # temperature [K]
-######################################################### make sure that the stoi_mat is fixed, the values here are just temporary
-stoi_mat = np.array([-1,-1,1,1])                            # [H2,CO, HC, H2O] 
+F_CO0 = 1239.6                  # [mol/s]
+F_H20 = 8.0 * F_CO0             # [mol/s]
+F_HC0 = 0.0                     # [mol/s]
+F_H2O0 = 0.0                    # [mol/s]
+
+F0 = np.array([F_H20, F_CO0, F_HC0, F_H2O0])        
+FT = F0.sum()           # total molar flow rate
+p = 20*1.01325                   # pressure for kinetics (units) [bar]
+pa= p * 1e5                   # pressure [Pa]
+T0 = 340 + 273.15             # temperature [K]
+
+Cp = np.array([ ])   # [H2, CO, HC, H2O]           # write these in the report later !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+stoi_mat = np.array([-2.0,-1.0,1.0,1.0])                            # [H2,CO, HC, H2O] 
 
 
 
@@ -63,19 +75,27 @@ def reactor(V:(float),vars:float, params:np.ndarray, stoi_mat:np.ndarray)->np.nd
     Returns: 
         dcdV                (np.ndarray): concentration ODE over the volume of the TFB reactor
     """
-    F, T = vars
-    Ea, Ru, DbH, a0, b0, P = params
+    F = vars[:-1]
+    T = vars[-1]
 
+    Ea, Ru, DbH, dHrxn, U, Tc, a0, b0, p, rho_cat_bed, F0 = params
+
+    # mass balance 
     FT = F.sum()
-    p_CO = F[1]/FT * P 
-    p_H2 = F[0]/FT * P
-
+    p_CO = F[1]/FT * p 
+    p_H2 = F[0]/FT * p
 
     alpha = a0 * np.exp((Ea/Ru)*(1/493.15 - 1/T))
     beta = b0 * np.exp((DbH/Ru)*(1/493.15 - 1/T))
-    r = (F*alpha*p_CO*p_H2)/(1 + beta * p_CO)**2
 
-    dcdV = stoi_mat.T * r
+    r_mass = (3*alpha*p_CO*p_H2)/(1 + beta * p_CO)**2        # taking activity of catalyst to be 3
+    r_vol = r_mass*rho_cat_bed
+
+    dcdV = stoi_mat * r_vol
+
+    # energy balance 
+    Cp_flow = F@Cp
+    dTdV = (-dHrxn * dcdV + U * (Tc - T))/(Cp_flow)
     
     return dcdV
 
